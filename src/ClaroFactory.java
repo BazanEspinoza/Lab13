ef20f088dfcac856d4100b8725e13db724d831e6
Susana Bazan <subaez2810@gmail.com>
2026-07-11 13:03:38 -0500

    feat: implementar ClaroFactory con sus tasas

--- src/ClaroFactory.java
+++ src/ClaroFactory.java
@@ -1,2 +1,22 @@
-public class ClaroFactory {
+public class ClaroFactory extends FabricaCreadora {
+
+    @Override
+    public PlanDatos crearPlanDatos(String tipo) {
+        PlanDatos plan = null;
+        switch (tipo.toLowerCase()) {
+            case "personal":
+                plan = new PlanDatosPersonal();
+                plan.setTasa(0.6f);
+                break;
+            case "negocio":
+                plan = new PlanDatosNegocio();
+                plan.setTasa(1.6f);
+                break;
+            case "academico":
+                plan = new PlanDatosAcademico();
+                plan.setTasa(1.2f);
+                break;
+        }
+        return plan;
+    }
 }
