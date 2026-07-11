0ed7184a7d54d2c245367c71e107f26904a87ead
Susana Bazan <subaez2810@gmail.com>
2026-07-11 13:25:32 -0500

    feat: agregar clase main PLanesDatosFactory

--- src/PlanesDatosFactory.java
+++ src/PlanesDatosFactory.java
@@ -2,14 +2,31 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class PlanesDatosFactory {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int megas = 1000;
+        System.out.println("BIENVENIDOS A LINEA RAPIDA");
+        FabricaCreadora lineaRapida = new LineaRapidaFactory();
+        calculoConsumo(lineaRapida, "personal", megas);
+        calculoConsumo(lineaRapida, "negocio", megas);
+        calculoConsumo(lineaRapida, "academico", megas);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("\nBIENVENIDOS A CLARO");
+        FabricaCreadora claro = new ClaroFactory();
+        calculoConsumo(claro, "personal", megas);
+        calculoConsumo(claro, "negocio", megas);
+        calculoConsumo(claro, "academico", megas);
+
+        System.out.println("\nBIENVENIDOS A MOVISTAR");
+        FabricaCreadora movistar = new MovistarFactory();
+        calculoConsumo(movistar, "personal", megas);
+        calculoConsumo(movistar, "negocio", megas);
+        calculoConsumo(movistar, "academico", megas);
+
+    }
+
+    private static void calculoConsumo(FabricaCreadora fabrica, String tipo, int megasConsumidos) {
+        PlanDatos plan = fabrica.crearPlanDatos(tipo);
+        float pago = plan.calculoPago(megasConsumidos);
+        System.out.println(plan.toString() + "Pago: " + pago + " por " + megasConsumidos + " megas");
     }
 }
+
