11f3254e9ceb65add2959bf543cd80c7fb7f2572
Susana Bazan <subaez2810@gmail.com>
2026-07-11 12:37:42 -0500

    feat: agregar clase abstracta PlanDatos con calculo de pago

--- src/PlanDatos.java
+++ src/PlanDatos.java
@@ -1,2 +1,16 @@
 public abstract class PlanDatos {
+    protected String nombre;
+    protected float tasa;
+
+    public void setTasa(float tasa) {
+        this.tasa = tasa;
+    }
+
+    public float calculoPago(int megasConsumidos) {
+        return megasConsumidos * tasa;
+    }
+    @Override
+    public String toString() {
+        return "Plan: " + nombre + "Tasa :" + tasa;
+    }
 }
