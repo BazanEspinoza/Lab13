0db4335d15bf8e5061c552a852115284e837840b
Susana Bazan <subaez2810@gmail.com>
2026-07-11 12:56:55 -0500

    feat: implementar LineaRapidaFactory con tasas de linea rapida

--- src/LineaRapidaFactory.java
+++ src/LineaRapidaFactory.java
@@ -1,2 +1,22 @@
-public class LineaRapidaFactory {
+public class LineaRapidaFactory extends FabricaCreadora {
+
+    @Override
+    public PlanDatos crearPlanDatos(String tipo) {
+        PlanDatos plan = null;
+        switch (tipo.toLowerCase()) {
+            case "personal":
+                plan =  new PlanDatosPersonal();
+                plan.setTasa(0.5f);
+                break;
+            case "negocio":
+                plan = new PlanDatosNegocio();
+                plan.setTasa(1.5f);
+                break;
+            case "academico":
+                plan = new PlanDatosAcademico();
+                plan.setTasa(1.0f);
+                break;
+        }
+        return null;
+    }
 }
